e185d047ba4f3dbb4eb6f77c71963eb94253892d
MVGIC <ex3ct@mail.ru>
2020-09-06 12:46:39 +0700

    main fixed

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        long InitialBalance = 100;
-        long DepositBalance = 600;
-        long Bonus;
-        long Limit = 1000;
+        long initialBalance = 100;
+        long depositBalance = 1050;
+        long bonus;
+        long limit = 1000;
 
 
-        if (DepositBalance <= Limit) {
-            Bonus = 0;
+        if (depositBalance <= limit) {
+            bonus = 0;
         } else {
-            Bonus = DepositBalance / 100;
+            bonus = depositBalance / 100;
         }
 
-        System.out.println(Bonus);
+        System.out.println(bonus);
     }
 }
